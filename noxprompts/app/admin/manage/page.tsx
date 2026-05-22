59e68b2e683214eeb0852a476446c8740387c9c0
Yusuf Shaikh | Developer <bhaiyusuf009@gmail.com>
2026-05-23 01:13:12 +0530

    Delete noxprompts directory

--- noxprompts/app/admin/manage/page.tsx
+++
@@ -1,9 +0,0 @@
-import { getAllTrends } from '@/lib/db';
-import ManageClient from '@/components/ManageClient';
-
-export const dynamic = 'force-dynamic';
-
-export default async function ManagePage() {
-  const trends = await getAllTrends();
-  return <ManageClient initialTrends={trends} />;
-}
