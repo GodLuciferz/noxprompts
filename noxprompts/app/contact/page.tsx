657f25faafb9be7e1711a7538955185db7fb6cd8
Yusuf Shaikh | Developer <bhaiyusuf009@gmail.com>
2026-06-03 15:52:29 +0530

    Update page.tsx

--- noxprompts/app/contact/page.tsx
+++ noxprompts/app/contact/page.tsx
@@ -1,8 +1,31 @@
 export default function ContactPage() {
   return (
-    <div className="p-6 max-w-3xl mx-auto">
+    <main style={{ maxWidth: 800, margin: "50px auto", padding: "20px" }}>
       <h1>Contact Us</h1>
-      <p>Email: support@noxzone111.online</p>
-    </div>
+
+      <p>
+        If you have any questions regarding our services, please contact us.
+      </p>
+
+      <h3>NoxPrompts</h3>
+
+      <p>EMAIL: munirashaikh180@gmail.com</p>
+      <p>ALT EMAIL: bhaiyusuf009@gmail.com</p>
+
+
+      <p>
+        Address:
+        <br />
+        BELA BIRAR, SITAMARHI 843333
+      </p>
+
+      <p>
+        Support Hours:
+        <br />
+        Monday - Saturday
+        <br />
+        10:00 AM - 6:00 PM
+      </p>
+    </main>
   );
 }
