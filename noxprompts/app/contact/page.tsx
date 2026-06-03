e5119d9a454de561d61bf45c4de68bf30b0f9ee4
Yusuf Shaikh | Developer <bhaiyusuf009@gmail.com>
2026-06-03 14:18:59 +0530

    Create page.tsx

---
+++ noxprompts/app/contact/page.tsx
@@ -0,0 +1,8 @@
+export default function ContactPage() {
+  return (
+    <div className="p-6 max-w-3xl mx-auto">
+      <h1>Contact Us</h1>
+      <p>Email: support@noxzone111.online</p>
+    </div>
+  );
+}
