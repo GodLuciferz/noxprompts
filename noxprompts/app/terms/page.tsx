048040667670fc61c095d5234e1a62b546154a22
Yusuf Shaikh | Developer <bhaiyusuf009@gmail.com>
2026-06-03 14:18:31 +0530

    Create page.tsx

---
+++ noxprompts/app/terms/page.tsx
@@ -0,0 +1,10 @@
+export default function TermsPage() {
+  return (
+    <div className="p-6 max-w-3xl mx-auto">
+      <h1>Terms & Conditions</h1>
+      <p>
+        By using NoxPrompts, you agree to these terms and conditions.
+      </p>
+    </div>
+  );
+}
