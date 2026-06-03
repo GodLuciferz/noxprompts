ce43bc93a9eb364849de9db0a9e05bdc934be66b
Yusuf Shaikh | Developer <bhaiyusuf009@gmail.com>
2026-06-03 14:18:00 +0530

    Create page.tsx

---
+++ noxprompts/app/privacy/page.tsx
@@ -0,0 +1,12 @@
+export default function PrivacyPage() {
+  return (
+    <div className="p-6 max-w-3xl mx-auto">
+      <h1>Privacy Policy</h1>
+      <p>
+        NoxPrompts respects your privacy. We may collect basic usage data to
+        improve our services.
+      </p>
+      <p>Email: support@noxzone111.online</p>
+    </div>
+  );
+}
